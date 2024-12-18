1ce7ff2d84546ce0a93ab6612ed611016c3defd9
Supun Mahima Herath <mahimasupun61@gmail.com>
2024-12-18 03:22:33 +0000

    updated index.php

--- html/index.php
+++ html/index.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="../css/style.css" />
   </head>
   <body>
-    <!-- Navbar -->
+    <!-- Navigation bar -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container">
         <a class="navbar-brand" href="../#"><i>Craft Culture</i></a>
