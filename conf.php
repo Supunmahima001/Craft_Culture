856aa25076ff4d05660b523454356c47eb0ca586
Supun Mahima Herath <mahimasupun61@gmail.com>
2024-12-31 12:06:27 +0000

    updated

--- conf.php
+++ conf.php
@@ -1,14 +1,14 @@
 <?php
 // Database connection
-$servername = "127.0.0.1"; // Your database host, e.g., localhost
-$username = "root"; // Your database username
-$password = "mariadb"; // Your database password (if any)
-$dbname = "craft_culture"; // The database name
+$servername = "127.0.0.1"; 
+$username = "root"; 
+$password = "mariadb"; 
+$dbname = "craft_culture"; 
 
-// Create connection
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
