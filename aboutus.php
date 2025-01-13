a51e3fe6255faa0ac673f18f9bde925cd73a557e
Supun Mahima Herath <mahimasupun61@gmail.com>
2025-01-13 16:18:53 +0000

    updated

--- aboutus.php
+++ aboutus.php
@@ -12,16 +12,61 @@
 </head>
 <body>
     
-    <nav>
-        <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="shop.php">Shop</a></li>
-            <li><a href="contact.php">Contact</a></li>
-            <li><a href="aboutus.php">About</a></li> 
-            <li><a href="signin.php">Sign In</a></li>
-            <li><a href="signup.php">Sign Up</a></li>
-           
-        </ul>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container">
+        <a class="navbar-brand" href="#"><i>Craft Culture</i></a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="index.php"
+                >Home</a
+              >
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="shop.php">Shop</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="aboutus.php">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="contact.php">Contact</a>
+            </li>
+
+            <li class="nav-item">
+              <a
+                class="nav-link"
+                href="signin.php"
+                style="margin-left: 4em; font-weight: bold"
+                >Sign in</a
+              >
+            </li>
+            <li class="nav-item">
+              <a
+                class="nav-link"
+                href="signup.php"
+                style="
+                  border: 1px solid;
+                  width: auto;
+                  font-weight: bold;
+                  margin-left: 1em;
+                "
+                >Signup</a
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
 
     
